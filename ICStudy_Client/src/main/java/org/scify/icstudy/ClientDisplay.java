d4131f7666ab6bc0b336f0f621744703e2d3ec1e
Paul Isaris <paulisaris@gmail.com>
2019-01-14 09:32:06 +0200

    Removed unused code and refactored

--- ICStudy_Client/src/main/java/org/scify/icstudy/ClientDisplay.java
+++ ICStudy_Client/src/main/java/org/scify/icstudy/ClientDisplay.java
@@ -14,9 +14,9 @@ public class ClientDisplay {
 
     private static final int SCR_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
     private static final int SCR_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
-    ICStudyCanvas canvas;
-    String sPort;
-    String ip;
+    protected ICStudyCanvas canvas;
+    protected String sPort;
+    protected String ip;
 
     public ClientDisplay(String ip, String sPort, ICStudyCanvas canvas) {
         this.ip = ip;
@@ -36,9 +36,6 @@ public class ClientDisplay {
         long lLastUpdate = 0L;
         System.out.println("before grabber.start()");
         grabber.start();
-//        while (grabber.grab() != null) {
-//            System.out.println("frame grabbed");
-//        }
         System.out.println("after grabber.start()");
         while (canvas.isDisplayable()) {
             //curImg = grabber.grab();
@@ -52,98 +49,24 @@ public class ClientDisplay {
             if (lNow - lLastUpdate < 500)
                 continue;
             lLastUpdate = lNow;
-
-            if (curImg != null) {
-                canvas.showImage(curImg);
-                // Flush the bufer with a 2% chance
-//                if (Math.random() < 0.02) {
-//                    // DEBUG LINES
-//                    System.out.println("Flushing buffer..");
-//                    //////////////
-//                    grabber.flush();
-//                }
-//                System.err.println("Received data...");
-            } else {
-                System.out.println("received a null img!");
-            }
+            showImage(grabber, curImg);
         }
         grabber.stop();
     }
 
-    public void listenAndDisplayTest() {
-        FFmpegFrameGrabber videoCapture = null;
-
-        String sListenTo = "udp://127.0.0.1:" + sPort;
-        System.err.println("Trying to open UDP port " + sListenTo + "...");
-
-        // https://github.com/opencv/opencv/pull/9292
-        // Windows patch
-        if (System.getProperty("os.name").toLowerCase().contains("win")) {
-            if (System.getenv("OPENCV_FFMPEG_CAPTURE_OPTIONS") != null &&
-                    !System.getenv("OPENCV_FFMPEG_CAPTURE_OPTIONS").equals("rtsp_transport;udp")) {
-
-                System.err.println("ERROR: FFMPEG environement variables should contain 'OPENCV_FFMPEG_CAPTURE_OPTIONS=rtsp_transport;udp' for Windows." +
-                        "\n Current value is : " +
-                        System.getenv("OPENCV_FFMPEG_CAPTURE_OPTIONS") + "\n Aborting...");
-                return;
+    protected void showImage(FrameGrabber grabber, opencv_core.IplImage curImg) throws FrameGrabber.Exception {
+        if (curImg != null) {
+            canvas.showImage(curImg);
+            // Flush the bufer with a 2% chance
+            if (Math.random() < 0.02) {
+                // DEBUG LINES
+                System.out.println("Flushing buffer..");
+                //////////////
+                grabber.flush();
             }
-        }
-
-        while (videoCapture == null) {
-            videoCapture = new FFmpegFrameGrabber(sListenTo);
-            try {
-                videoCapture.start();
-            } catch (FrameGrabber.Exception e) {
-                System.err.println("Could not open video stream. Reason:");
-                e.printStackTrace(System.err);
-                return;
-            }
-        }
-
-//            try {
-//                if (!videoCapture.isOpened()) {
-//                    Thread.sleep(1000);
-//                    System.err.println("Retrying...");
-//                }
-//
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//
-//        boolean isOpened = videoCapture.isOpened();
-//        boolean isSucceed = videoCapture.grab();
-//
-//        System.out.println("isOpened: " + isOpened);
-//        System.out.println("isSucceed: " + isSucceed);
-        System.out.println("------- START GRAB -------");
-        while (canvas.isDisplayable()) {
-
-            Frame fCur = null;
-            try {
-                fCur = videoCapture.grab();
-            } catch (FrameGrabber.Exception e) {
-                e.printStackTrace();
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-                continue;
-            }
-//            opencv_core.Mat mat = new OpenCVFrameConverter.ToMat().convertToMat(fCur);
-//            opencv_core.IplImage iplImage = Java2DFrameUtils.toIplImage(mat);
-            opencv_core.IplImage iplImage = new OpenCVFrameConverter.ToMat().convertToIplImage(fCur);
-            System.out.println("width, height = "+iplImage.width()+", "+iplImage.height());
-            if(iplImage != null) {
-                canvas.showImage(iplImage);
-                System.err.println("Received data...");
-            } else {
-                System.out.println("received a null img!");
-            }
+            System.err.println("Received data...");
+        } else {
+            System.out.println("received a null img!");
         }
     }
-
-
 }
